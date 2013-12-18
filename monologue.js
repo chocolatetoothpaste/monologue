b82febcb855320c806ddfee3c0e451c4f5034156
Ross Paskett <ross@paskett.me>
2013-12-18 13:08:41 -0700

    Fixing a ciritcal issue with query construction

--- monologue.js
+++ monologue.js
@@ -1,7 +1,7 @@
 (function() {
 	var rx = /[^a-zA-Z0-9_]/g,
 		root = this,
-		yoe = {
+		qparts = {
 			query: '',
 			where: '',
 			having: '',
@@ -21,18 +21,21 @@
 		root.monologue = new Monologue;
 	}
 
+
 	/**
-	 * Creates a select statement using properties defined before calling method
-	 * @return string	the generated query string
 	 */
 
 	Monologue.prototype.select = function( c, t ) {
 		c = ( typeof c === "string" ? c : c.join(", ") );
 
-		yoe.query = "SELECT " + c + " FROM " + t;
+		qparts.query = "SELECT " + c + " FROM " + t;
 
 		return this;
 	}
+
+
+	/**
+	 */
 
 	Monologue.prototype.where = function( w, separator ) {
 		separator = ( typeof separator === "undefined" ? "AND" : separator );
@@ -51,12 +54,16 @@
 		}
 
 		// check if a previous where statement has been set and glue it all together
-		yoe.where = ( yoe.where.length > 0
-			? yoe.where + " " + separator + " " + w
+		qparts.where = ( qparts.where.length > 0
+			? qparts.where + " " + separator + " " + w
 			: w );
 
 		return this;
 	}
+
+
+	/**
+	 */
 
 	Monologue.prototype.having = function( h, separator ) {
 		separator = ( typeof separator === "undefined" ? "AND" : separator );
@@ -75,12 +82,16 @@
 		}
 
 		// check if a previous where statement has been set and glue it all together
-		yoe.having = ( yoe.having.length > 0
-			? yoe.having + " " + separator + " " + h
+		qparts.having = ( qparts.having.length > 0
+			? qparts.having + " " + separator + " " + h
 			: h );
 
 		return this;
 	}
+
+
+	/**
+	 */
 
 	Monologue.prototype.in = function( ins )
 	{
@@ -99,6 +110,10 @@
 		// returns "this"
 		return this.where( "IN (" + i + ")", "" );
 	}
+
+
+	/**
+	 */
 
 	Monologue.prototype.like = function( like, separator ) {
 		separator = separator || "AND";
@@ -120,6 +135,10 @@
 
 		return this.where( like, "" );
 	}
+
+
+	/**
+	 */
 
 	Monologue.prototype.between = function( one, two )
 	{
@@ -143,34 +162,46 @@
 		return this.where( " BETWEEN :" + k1 + " AND :" + k2, '' );
 	}
 
-	// group, direction
+
+	/**
+	 * group, direction
+	 */
+
 	Monologue.prototype.group = function( g, d ) {
 		d = d || 'ASC';
 
 		if( typeof g !== "string" )
 			g = g.join( ',' );
 
-		yoe.group.push( g + " " + d );
+		qparts.group.push( g + " " + d );
 
 		return this;
 	}
 
-	// order, direction
+
+	/**
+	 * order, direction
+	 */
+
 	Monologue.prototype.order = function( o, d ) {
 		d = d || 'ASC';
 
 		if( typeof o !== "string" )
 			o = o.join( ',' );
 
-		yoe.order.push( o + " " + d );
+		qparts.order.push( o + " " + d );
 
 		return this;
 	}
 
-	// limit, offset
+
+	/**
+	 * limit, offset
+	 */
+
 	Monologue.prototype.limit = function( l, o )
 	{
-		yoe.limit = ( typeof o === "undefined" ? l : o + ", " + l );
+		qparts.limit = ( typeof o === "undefined" ? l : o + ", " + l );
 		return this;
 	}
 
@@ -181,85 +212,22 @@
 
 	Monologue.prototype.query = function()
 	{
-		if( yoe.where.length > 0 )
-			yoe.query = yoe.query + " WHERE " + yoe.where;
-		if( yoe.group.length > 0 )
-			yoe.query += ' GROUP BY ' + yoe.group.join(',');
-		if( yoe.having.length > 0 )
-			yoe.query = yoe.query + " HAVING " + yoe.having;
-		if( yoe.order.length > 0 )
-			yoe.query += ' ORDER BY ' + yoe.order.join(',');
-		if( yoe.limit.length > 0 )
-			yoe.query += " LIMIT " + yoe.limit;
+		if( qparts.where.length > 0 )
+			qparts.query += " WHERE " + qparts.where;
+		if( qparts.group.length > 0 )
+			qparts.query += " GROUP BY " + qparts.group.join(',');
+		if( qparts.having.length > 0 )
+			qparts.query += " HAVING " + qparts.having;
+		if( qparts.order.length > 0 )
+			qparts.query += " ORDER BY " + qparts.order.join(',');
+		if( qparts.limit.length > 0 )
+			qparts.query += " LIMIT " + qparts.limit;
 
-		return yoe.query;
+		return qparts.query;
 	}
 
 
 	/**
-	 * Return a generated insert statement from one or more sets of values
-	 * passed as an associative array. Inserting a batch of rows rather than
-	 * multiple consecutive inserts will be WAY faster.
-	 * @param string $table
-	 * @param array $values
-	 * @return string
-	 */
-
-	// Monologue.prototype.insert = function( $table, array $params )
-	// {
-	// 	$columns = array();
-	// 	$multi = ( count( $params ) != count( $params, COUNT_RECURSIVE ) );
-	// 	if( $multi )
-	// 	{
-	// 		$arr = array();
-	// 		foreach( $params as $k => $void )
-	// 		{
-	// 			// sort the keys so that each group of values are inserted in
-	// 			// the same order
-	// 			ksort( $void );
-	// 			if( ! $columns )
-	// 			{
-	// 				$columns = array_keys( $params[0] );
-	// 				sort( $columns );
-	// 			}
-
-	// 			foreach( $void as $pkey => $pvalue )
-	// 			{
-	// 				$this->params[$pkey . $k] = $pvalue;//sprintf( ':%s', $pvalue );
-	// 				$arr[] = ":{$pkey}{$k}";
-	// 			}
-
-	// 			$params[$k] = implode( ', ', $arr );
-	// 			$params[$k] = sprintf( '(%s)', $params[$k] );
-	// 			$arr = array();
-	// 		}
-
-	// 		$params = implode( ', ', $params );
-
-	// 	}
-	// 	else
-	// 	{
-	// 		ksort( $params );
-	// 		$this->params = $params;
-	// 		$columns = array_keys( $this->params );
-	// 		$params = asprintf( ':%1$s', $columns );
-	// 		$params = implode( ', ', $params );
-	// 		$params = sprintf( '(%s)', $params );
-	// 	}
-
-	// 	$columns = sprintf( '(%s)', '`' . implode( '`, `', $columns ) . '`' );
-	// 	$this->query = "INSERT INTO $table $columns VALUES $params";
-
-	// 	return $this;
-	// }
-
-
-	/**
-	 * Generates an update statement
-	 * @param string $table
-	 * @param array $params
-	 * @param array $where
-	 * @return string
 	 */
 
 	Monologue.prototype.insert = function( table, params ) {
@@ -299,7 +267,7 @@
 				values[v] = "(" + values[v].join(',') + ")";
 			}
 
-			columns = "  (" + columns.join(', ') + ") VALUES " + values.join(',');
+			columns = " (" + columns.join(', ') + ") VALUES " + values.join(',');
 
 		}
 
@@ -307,18 +275,13 @@
 			var columns = " " + params;
 		}
 
-		this.query = "INSERT INTO " + table + columns;
+		qparts.query = "INSERT INTO " + table + columns;
 
 		return this;
 	};
 
 
 	/**
-	 * Generates an update statement
-	 * @param string $table
-	 * @param array $params
-	 * @param array $where
-	 * @return string
 	 */
 
 	Monologue.prototype.update = function( table, params ) {
@@ -338,21 +301,21 @@
 			columns = " " + params;
 		}
 
-		this.query = "UPDATE " + table + columns;
+		qparts.query = "UPDATE " + table + columns;
 
 		return this;
 	};
 
 
 	/**
-	 * Creates a select statement using properties defined before calling method
-	 * @return string	the generated query string
 	 */
 
-	Monologue.prototype.delete = function( t )
+	Monologue.prototype.delete = function( table, where )
 	{
-		yoe.table = t;
-		yoe.query = "DELETE FROM " + yoe.table;
+		qparts.query = "DELETE FROM " + table;
+
+		if( where )
+			this.where(where);
 
 		return this;
 	};
