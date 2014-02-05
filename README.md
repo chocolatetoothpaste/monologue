24f9e3dd6ea77429d6f894e366fa47b52db8faec
Ross Paskett <ross@paskett.me>
2014-02-05 11:14:21 -0700

    Wrapping constructor so multiple instances can be had, and encapsulating functions differently

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Monologue - Streamlined query building
 ======================================
 
+*Breaking Change - it has become obvious that in some cases, multiple instances of monologue may be used in a single script.  To accommodate this, the constructor is now deployed through a wrapper.  See examples below for correct usage.
+
 **Install**
 
     npm install monologue
@@ -12,17 +14,20 @@ This was ported from a PHP library, and uses named parameters for binding (PDO l
     var mono = require('monologue');
 
     // Less than basic SELECT statement
-    // output: SELECT * FROM users WHERE id IN (:i_1,:i_2,:i_3,:i_4,:i_5,:i_6) AND username = :e_someguy AND email = :e_someguyexampleorg OR email = :e_someguygmailcom AND date BETWEEN :b_20120912 AND :b_20130121 AND name LIKE :l_roen OR name LIKE :l_bb GROUP BY type ASC ORDER BY id ASC LIMIT 1000, 300
+
 
     // call the SQL wrappers in any order, see below: where, group, where, order
-    mono.select( "*", "users")
+    var mono = monologue().select( "*", "users")
         .where( { "id": [1,2,3,4,5,6] } ) // alternative to where("id").in([...])
         .where( 'date_time' ).between( '2012-09-12', '2013-01-20')
         .group( 'type' )
         .where( "name", "OR" ).like("ro%en") // out of order, also passing "OR" as separator
         .order( "id" )
         .limit( '300', 1000 )
-        .query().sql;
+        .query();
+
+    console.log( mono.sql )
+    // output: SELECT * FROM users WHERE id IN (:i_1,:i_2,:i_3,:i_4,:i_5,:i_6) AND username = :e_someguy AND email = :e_someguyexampleorg OR email = :e_someguygmailcom AND date BETWEEN :b_20120912 AND :b_20130121 AND name LIKE :l_roen OR name LIKE :l_bb GROUP BY type ASC ORDER BY id ASC LIMIT 1000, 300
 
     console.log( mono.params );
     /* output:
@@ -43,41 +48,43 @@ This was ported from a PHP library, and uses named parameters for binding (PDO l
     // JOIN (default is left):
     // SELECT * FROM users u LEFT JOIN posts p ON p.user_id = u.id WHERE category = :e_67
 
-    monologue.select( "*", "users u" )
+    monologue().select( "*", "users u" )
         .join( "posts p", "p.user_id = u.id" )
-        .where( { "category": "67" } );
+        .where( { "category": "67" } )
+        .query().sql;
 
 
     // JOIN (INNER, as argument):
     // SELECT * FROM users u INNER JOIN posts p ON p.user_id = u.id WHERE category = :e_67
 
-    monologue.select( "*", "users u" )
+    monologue().select( "*", "users u" )
         .join( "INNER", "posts p", { "p.user_id": "u.id" } )
-        .where( { "category": "67" } );
+        .where( { "category": "67" } )
+        .query().sql;
 
 
     // SELECT into outfile: the third param (OPTIONALLY ENCLOSED BY) is, as stated, optional. Just pass in the line ending and leave the 4th param out, the rest will be taken care of
     // output: SELECT * FROM users WHERE company = :e_generalmotors INTO OUTFILE '/tmp/datafile' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
 
-    monologue.select( "*", "users" )
+    monologue().select( "*", "users" )
         .where( { "company": "general motors" } )
         .file( "/tmp/datafile", ",", '"', "\\n" )
-        .query();
+        .query().sql;
 
 
     // SELECT into outfile: without third param
     // output: SELECT * FROM users WHERE company = :e_generalmotors INTO OUTFILE '/tmp/datafile' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
 
-    monologue.select( "*", "users")
+    monologue().select( "*", "users")
         .where( { "company": "general motors" } )
         .file( "/tmp/datafile", ",", "\\n" )
-        .query();
+        .query().sql;
 
 
     // INSERT, passing an array of objects
     // output: INSERT INTO users (username,password,first_name) VALUES (:e_test,:e_1234,:e_me),(:e_example,:e_abcd,:e_rasta)
 
-    monologue.insert( 'users', [
+    monologue().insert( 'users', [
         { username: 'test', password: '1234', first_name: 'me' },
         { username: 'example', password: 'abcd', first_name: "rasta" }
     ] ).query().sql
@@ -86,16 +93,16 @@ This was ported from a PHP library, and uses named parameters for binding (PDO l
     // INSERT, passing a single object
     // output: INSERT INTO users (username,password,first_name) VALUES (:e_me,:e_abcd,:e_cubert)
 
-    monologue.insert( 'users', { username: 'me', password: 'abcd', first_name: "cubert" } ).query().sql
+    monologue().insert( 'users', { username: 'me', password: 'abcd', first_name: "cubert" } ).query().sql
 
 
     // UPDATE
     // output: UPDATE users SET username = :e_yoyo, email = :e_kay, password = :e_abcdefg WHERE id = :e_23
 
-    monologue.update( "users", {username: "yoyo", email: 'some@email.com', password: "abcdefg"} ).where( {id: 23} ).query().sql
+    monologue().update( "users", {username: "yoyo", email: 'some@email.com', password: "abcdefg"} ).where( {id: 23} ).query().sql
 
 
     // DELETE
     // output: DELETE FROM users WHERE username = :e_test AND password = :e_1234 AND first_name = :e_me
 
-    monologue.delete( 'users', { username: 'test', password: '1234', first_name: "me" } ).query().sql;
+    monologue().delete( 'users', { username: 'test', password: '1234', first_name: "me" } ).query().sql;
--- monologue.js
+++ monologue.js
@@ -1,23 +1,9 @@
 (function() {
 	var rx = /[^a-zA-Z0-9_]/g,
-		root = this,
-		global = {
-			query: '',
-			join: [],
-			where: '',
-			having: '',
-			order: [],
-			group: [],
-			limit: '',
-			last: ''
-		};
-
-	function Monologue() {
-		this.params = {}, this.sql = '';
-	};
+		root = this;
 
 	function monologue() {
-		global = {
+		var global = {
 			query: '',
 			join: [],
 			where: '',
@@ -28,383 +14,362 @@
 			last: ''
 		};
 
-		return new Monologue;
+		return {
+			params: {},
+			sql: '',
+
+			/**
+			 */
+
+			select: function( c, t ) {
+				if( toString.call( c ) === "[object Array]" )
+					c = c.join( ", " );
+
+				global.query = "SELECT " + c + " FROM " + t;
+
+				return this;
+			},
+
+
+			/**
+			 * d: direction, t: table, f: fields
+			 */
+
+			join: function( d, t, f ) {
+				if( typeof f === "undefined" ) {
+					f = t;
+					t = d;
+					d = "LEFT";
+				}
+
+				if( typeof f === "object" ) {
+					var fields = [];
+					for( var ii in f ) {
+						fields.push( ii + " = " + f[ii] );
+					}
+
+					f = fields.join(" AND ");
+				}
+
+				global.join.push( " " + d + " JOIN " + t + " ON " + f );
+
+				return this;
+			},
+
+
+			/**
+			 */
+
+			insert: function( table, params ) {
+				// I don't know why this would ever NOT be ther case
+				if( typeof params === "object" ) {
+					var columns = [], values = [], index = 0;
+
+					// if it's not a multidimensional array, cheat and make it one
+					if( toString.call( params ) !== "[object Array]" ) {
+						params = [params];
+					}
+
+					columns = this.stringify( params, "");
+					columns = "(" + columns.shift() + ") VALUES "
+						+ columns.join(',');
+				}
+
+				else if( typeof params === "string" ) {
+					var columns = params;
+				}
+
+				else {
+					// emit error
+				}
+
+				global.query = "INSERT INTO " + table + " " + columns;
+
+				return this;
+			},
+
+
+			/**
+			 */
+
+			update: function( table, params ) {
+				if( typeof params === "object" ) {
+					var columns = this.stringify(params);
+					columns = "SET " + columns.join( ', ' );
+				}
+
+				else if( typeof params === "string" ) {
+					var columns = params;
+				}
+
+				else {
+					// throw error
+				}
+
+				global.query = "UPDATE " + table + " " + columns;
+
+				return this;
+			},
+
+
+			/**
+			 */
+
+			delete: function( table, where ) {
+				global.query = "DELETE FROM " + table;
+				return ( where ? this.where( where ) : this );
+			},
+
+
+			/**
+			 * w: where statement, s: separator
+			 */
+
+			where: function( w, s ) {
+				s = ( typeof s === "undefined" ? "AND" : s );
+				s = ( s.length > 0 ? " " + s + " " : s );
+
+				// console.log(w)
+
+				if( toString.call( w ) === "[object Object]" ) {
+					var criteria = this.stringify( w );
+
+					// stringify the where statements
+					w = criteria.join( s );
+				}
+
+				// check if a previous where statement has been set and glue it
+				// all together
+				global.where = ( global.where.length > 0
+					? global.where + s + w
+					: w );
+
+				return this;
+			},
+
+
+			/**
+			 */
+
+			in: function( ins ) {
+				var i = this.stringify( ins, '' );
+
+				// returns "this"
+				return this.where( " IN (" + i.join( "," ) + ")", "" );
+			},
+
+
+			/**
+			 */
+
+			like: function( like, separator ) {
+				separator = separator || "AND";
+
+				// calling this.where() will take of stringifying, so gluing the
+				// statement together is all that needs to be done here
+				var k = "l_" + like.replace(rx, '');
+				this.params[k] = like;
+				like = " LIKE :" + k;
+
+				return this.where( like, "" );
+			},
+
+
+			/**
+			 */
+
+			between: function( one, two ) {
+				// create unique field names for each value
+				var k1 = "b_" + one.replace(rx, "");
+				var k2 = "b_" + two.replace(rx, "");
+
+				this.params[k1] = one;
+				this.params[k2] = two;
+
+				return this.where( " BETWEEN :" + k1 + " AND :" + k2, '' );
+			},
+
+
+			/**
+			 * g: group, d: direction
+			 */
+
+			group: function( g, d ) {
+				d = d || 'ASC';
+
+				if( toString.call( g ) === "[object Array]" ) g = g.join( ',' );
+
+				global.group.push( g + " " + d );
+
+				return this;
+			},
+
+
+			/**
+			 * h: having
+			 */
+
+			having: function( h, separator ) {
+				separator = ( typeof separator === "undefined" ? "AND" : separator );
+
+				if( typeof h !== "string" ) {
+					var criteria = this.stringify(h);
+
+					// stringify the having statements
+					h = criteria.join( " " + separator + " " );
+				}
+
+				// check if a previous statement has been set and glue it
+				// all together
+				global.having = ( global.having.length > 0
+					? global.having + " " + separator + " " + h
+					: h );
+
+				return this;
+			},
+
+
+			/**
+			 * o: order, d: direction
+			 */
+
+			order: function( o, d ) {
+				d = d || 'ASC';
+
+				if( toString.call( o ) === "[object Array]" ) o = o.join( ',' );
+
+				global.order.push( o + " " + d );
+
+				return this;
+			},
+
+
+			/**
+			 * l: limit, o: offset
+			 */
+
+			limit: function( l, o ) {
+				global.limit = ( typeof o === "undefined" ? l : o + ", " + l );
+				return this;
+			},
+
+
+			/**
+			 * f: file path, t: field terminator, e: field enclosure, l: line terminator
+			 */
+
+			file: function( f, t, e, l ) {
+				if( typeof l === "undefined" ) {
+					l = e;
+					e = undefined;
+				}
+
+				global.last += " INTO OUTFILE '" + f
+					+ "' FIELDS TERMINATED BY '" + t + "' "
+					+ ( e ? "OPTIONALLY ENCLOSED BY '" + e + "'" : '' )
+					+ " LINES TERMINATED BY '" + l + "'";
+
+				return this;
+			},
+
+
+			/**
+			 * Compile each part together and generate a valid SQL statement
+			 */
+
+			query: function() {
+				if( global.join.length > 0 )
+					global.query += global.join.join();
+				if( global.where.length > 0 )
+					global.query += " WHERE " + global.where;
+				if( global.group.length > 0 )
+					global.query += " GROUP BY " + global.group.join(',');
+				if( global.having.length > 0 )
+					global.query += " HAVING " + global.having;
+				if( global.order.length > 0 )
+					global.query += " ORDER BY " + global.order.join(',');
+				if( global.limit.length > 0 )
+					global.query += " LIMIT " + global.limit;
+				if( global.last.length > 0 )
+					global.query += global.last;
+
+				this.sql = global.query;
+
+				return this;
+			},
+
+
+			/**
+			 * p: params, s: separator, pre: bound param prefix
+			 */
+
+			stringify: function( p, s ) {
+				s = ( typeof s === "undefined" ? "=" : s );
+				var c = [],
+					type = toString.call( p );
+
+				if( type === "[object Array]" ) {
+					for( var ii = 0, l = p.length; ii < l; ++ii ) {
+
+						// if parent is an array and child is an object,
+						// generate an encapsulated list of values (for inserts)
+						if( toString.call( p[ii] ) === "[object Object]" ) {
+
+							// if "c" is empty, push actual column names
+							if( c.length === 0 ) c.push( Object.keys( p[ii] ) );
+
+							c.push( "(" + this.stringify( p[ii], "" ) + ")");
+						}
+
+						else {
+
+							// generate a comma-separated list of fields
+							c.push( this.format( p[ii], ii, "" ) );
+						}
+					}
+				}
+
+				else {
+					for( var jj in p ) {
+						if( toString.call( p[jj] ) === "[object Array]" ) {
+							c.push( jj + " IN (" + this.stringify( p[jj] ) + ")" );
+						}
+
+						else {
+							c.push( this.format( p[jj], jj, s ) );
+						}
+					}
+				}
+
+				return c;
+			},
+
+
+			/**
+			 */
+
+			format: function( v, k, s ) {
+				// strip out non-alpha characters (makes parsers choke)
+				var push = v.toString().replace( rx, "" );
+				var r = "e_" + push;
+
+				// add value to the param stack
+				this.params[r] = v;
+
+				// spit out the bound param name
+				return ( s.length > 0 ? k + " " + s + " " : '' ) + ":" + r;
+			}
+		}
 	}
 
 	if( typeof module !== "undefined" && module.exports ) {
-		module.exports = new Monologue;
-		// module.exports = monologue;
+		// module.exports = new Monologue;
+		module.exports = monologue;
 	}
 	else {
-		root.monologue = new Monologue;
-		// root.monologue = monologue;
+		// root.monologue = new Monologue;
+		root.monologue = monologue;
 	}
 
-
-	/**
-	 */
-
-	Monologue.prototype.select = function( c, t ) {
-		this.reset();
-
-		if( toString.call( c ) === "[object Array]" )
-			c = c.join( ", " );
-
-		global.query = "SELECT " + c + " FROM " + t;
-
-		return this;
-	}
-
-
-	/**
-	 * d: direction, t: table, f: fields
-	 */
-
-	Monologue.prototype.join = function( d, t, f ) {
-		if( typeof f === "undefined" ) {
-			f = t;
-			t = d;
-			d = "LEFT";
-		}
-
-		if( typeof f === "object" ) {
-			var fields = [];
-			for( var ii in f ) {
-				fields.push( ii + " = " + f[ii] );
-			}
-
-			f = fields.join(" AND ");
-		}
-
-		global.join.push( " " + d + " JOIN " + t + " ON " + f );
-
-		return this;
-	}
-
-
-	/**
-	 */
-
-	Monologue.prototype.insert = function( table, params ) {
-		this.reset();
-
-		// I don't know why this would ever NOT be ther case
-		if( typeof params === "object" ) {
-			var columns = [], values = [], index = 0;
-
-			// if it's not a multidimensional array, cheat and make it one
-			if( toString.call( params ) !== "[object Array]" ) {
-				params = [params];
-			}
-
-			columns = this.stringify( params, "");
-			columns = "(" + columns.shift() + ") VALUES " + columns.join(',');
-		}
-
-		else if( typeof params === "string" ) {
-			var columns = params;
-		}
-
-		else {
-			// emit error
-		}
-
-		global.query = "INSERT INTO " + table + " " + columns;
-
-		return this;
-	};
-
-
-	/**
-	 */
-
-	Monologue.prototype.update = function( table, params ) {
-		this.reset();
-
-		if( typeof params === "object" ) {
-			var columns = this.stringify(params);
-			columns = "SET " + columns.join( ', ' );
-		}
-
-		else if( typeof params === "string" ) {
-			var columns = params;
-		}
-
-		else {
-			// throw error
-		}
-
-		global.query = "UPDATE " + table + " " + columns;
-
-		return this;
-	};
-
-
-	/**
-	 */
-
-	Monologue.prototype.delete = function( table, where ) {
-		this.reset();
-
-		global.query = "DELETE FROM " + table;
-		return ( where ? this.where( where ) : this );
-	};
-
-
-	/**
-	 * w: where statement, s: separator
-	 */
-
-	Monologue.prototype.where = function( w, s ) {
-		s = ( typeof s === "undefined" ? "AND" : s );
-		s = ( s.length > 0 ? " " + s + " " : s );
-
-		// console.log(w)
-
-		if( toString.call( w ) === "[object Object]" ) {
-			var criteria = this.stringify( w );
-
-			// stringify the where statements
-			w = criteria.join( s );
-		}
-
-		// check if a previous where statement has been set and glue it all together
-		global.where = ( global.where.length > 0
-			? global.where + s + w
-			: w );
-
-		return this;
-	}
-
-
-	/**
-	 */
-
-	Monologue.prototype.in = function( ins ) {
-		var i = this.stringify( ins, '' );
-
-		// returns "this"
-		return this.where( " IN (" + i.join( "," ) + ")", "" );
-	}
-
-
-	/**
-	 */
-
-	Monologue.prototype.like = function( like, separator ) {
-		separator = separator || "AND";
-
-		// calling this.where() will take of stringifying, so gluing the
-		// statement together is all that needs to be done here
-		var k = "l_" + like.replace(rx, '');
-		this.params[k] = like;
-		like = " LIKE :" + k;
-
-		return this.where( like, "" );
-	}
-
-
-	/**
-	 */
-
-	Monologue.prototype.between = function( one, two ) {
-		// create unique field names for each value
-		var k1 = "b_" + one.replace(rx, "");
-		var k2 = "b_" + two.replace(rx, "");
-
-		this.params[k1] = one;
-		this.params[k2] = two;
-
-		return this.where( " BETWEEN :" + k1 + " AND :" + k2, '' );
-	}
-
-
-	/**
-	 * g: group, d: direction
-	 */
-
-	Monologue.prototype.group = function( g, d ) {
-		d = d || 'ASC';
-
-		if( toString.call( g ) === "[object Array]" ) g = g.join( ',' );
-
-		global.group.push( g + " " + d );
-
-		return this;
-	};
-
-
-	/**
-	 * h: having
-	 */
-
-	Monologue.prototype.having = function( h, separator ) {
-		separator = ( typeof separator === "undefined" ? "AND" : separator );
-
-		if( typeof h !== "string" ) {
-			var criteria = this.stringify(h);
-
-			// stringify the having statements
-			h = criteria.join( " " + separator + " " );
-		}
-
-		// check if a previous where statement has been set and glue it all together
-		global.having = ( global.having.length > 0
-			? global.having + " " + separator + " " + h
-			: h );
-
-		return this;
-	}
-
-
-	/**
-	 * o: order, d: direction
-	 */
-
-	Monologue.prototype.order = function( o, d ) {
-		d = d || 'ASC';
-
-		if( toString.call( o ) === "[object Array]" ) o = o.join( ',' );
-
-		global.order.push( o + " " + d );
-
-		return this;
-	}
-
-
-	/**
-	 * l: limit, o: offset
-	 */
-
-	Monologue.prototype.limit = function( l, o ) {
-		global.limit = ( typeof o === "undefined" ? l : o + ", " + l );
-		return this;
-	}
-
-
-	/**
-	 * f: file path, t: field terminator, e: field enclosure, l: line terminator
-	 */
-
-	Monologue.prototype.file = function( f, t, e, l ) {
-		if( typeof l === "undefined" ) {
-			l = e;
-			e = undefined;
-		}
-
-		global.last += " INTO OUTFILE '" + f + "' FIELDS TERMINATED BY '"
-			+ t + "' " + ( e ? "OPTIONALLY ENCLOSED BY '" + e + "'" : '' )
-			+ " LINES TERMINATED BY '" + l + "'";
-
-		return this;
-	};
-
-
-	/**
-	 * Compile each part together and generate a valid SQL statement
-	 */
-
-	Monologue.prototype.query = function() {
-		if( global.join.length > 0 )
-			global.query += global.join.join();
-		if( global.where.length > 0 )
-			global.query += " WHERE " + global.where;
-		if( global.group.length > 0 )
-			global.query += " GROUP BY " + global.group.join(',');
-		if( global.having.length > 0 )
-			global.query += " HAVING " + global.having;
-		if( global.order.length > 0 )
-			global.query += " ORDER BY " + global.order.join(',');
-		if( global.limit.length > 0 )
-			global.query += " LIMIT " + global.limit;
-		if( global.last.length > 0 )
-			global.query += global.last;
-
-		this.sql = global.query;
-
-		return this;
-	}
-
-
-	/**
-	 * p: params, s: separator, pre: bound param prefix
-	 */
-
-	Monologue.prototype.stringify = function( p, s ) {
-		s = ( typeof s === "undefined" ? "=" : s );
-		var c = [],
-			type = toString.call( p );
-
-		if( type === "[object Array]" ) {
-			for( var ii = 0, l = p.length; ii < l; ++ii ) {
-
-				// if parent is an array and child is an object, generate an
-				// encapsulated list of values (for insert statements)
-				if( toString.call( p[ii] ) === "[object Object]" ) {
-
-					// if "c" is empty, then push the actual column names
-					if( c.length === 0 ) c.push( Object.keys( p[ii] ) );
-
-					c.push( "(" + this.stringify( p[ii], "" ) + ")");
-				}
-
-				else {
-
-					// generate a comma-separated list of fields
-					c.push( this.format( p[ii], ii, "" ) );
-				}
-			}
-		}
-
-		else {
-			for( var jj in p ) {
-				if( toString.call( p[jj] ) === "[object Array]" ) {
-					c.push( jj + " IN (" + this.stringify( p[jj] ) + ")" );
-				}
-
-				else {
-					c.push( this.format( p[jj], jj, s ) );
-				}
-			}
-		}
-
-		return c;
-	}
-
-
-	/**
-	 */
-
-	Monologue.prototype.format = function( v, k, s ) {
-		// strip out non-alpha characters (makes parsers choke)
-		var push = v.toString().replace( rx, "" );
-		var r = "e_" + push;
-
-		// add value to the param stack
-		this.params[r] = v;
-
-		// spit out the bound param name
-		return ( s.length > 0 ? k + " " + s + " " : '' ) + ":" + r;
-	}
-
-
-	/**
-	 * Reset all placeholders so next query isn't polluted
-	 */
-
-	Monologue.prototype.reset = function() {
-		this.params = {};
-
-		global = {
-			query: '',
-			join: [],
-			where: '',
-			group: [],
-			having: '',
-			order: [],
-			limit: '',
-			last: ''
-		};
-	}
 
 })();
